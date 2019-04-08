2b49d6a0ffeebe829e7ab2b748dab5e5ae09355a
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-04-07 23:24:18 -0400

    updated page

--- App/routing/api-routes.js
+++ App/routing/api-routes.js
@@ -1,37 +1,33 @@
 var friends = require("../data/friends.js");
-
 module.exports = function(app)  {
     app.get("/api/friends", function(req, res)  {
-        res.json(friends);
-    });
+    res.json(friends);
+});
 
-    app.post("/api/friends", function(req, res)   {
-        var pointDifference = 0;
-        var idealMatch =    {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
-        var userData = req.body;
-        var userData = userData.name;
-        var userScores = userData.scores;
-
-        var b = userScores.map(function(item)   {
-            return parseInt(item, 10);
-        });
-        userData = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores: b
-        };
-
-        console.log("Name:" + userName);
-        console.log("User score" + userScores);
-
-        var sum = b.reduce(a, b) => a + b, 0);
-        console.log("Sum of users score" + sum);
-        console.log("Best Match Friend Diff" + idealMatch.friendDifference);
-        console.log("++++++++++++++=========================================");
+app.post("/api/friends", function(req, res)   {
+    var pointDifference = 0;
+    var idealMatch =    {
+        name: "",
+        photo: "",
+        friendDifference: 1000
+};
+    var userData = req.body;
+    var userData = userData.name;
+    var userScores = userData.scores;
+    var b = userScores.map(function(item)   {
+    return parseInt(item, 10);
+});
+userData = {
+    name: req.body.name,
+    photo: req.body.photo,
+    scores: b
+    };
+console.log("Name:" + userName);
+console.log("User score" + userScores);
+var sum = b.reduce(a, b) => a + b, 0);
+console.log("Sum of users score" + sum);
+console.log("Best Match Friend Diff" + idealMatch.friendDifference);
+console.log("++++++++++++++=========================================");
 
 
         for (var i = 0; i < friends.length; i++)    {
